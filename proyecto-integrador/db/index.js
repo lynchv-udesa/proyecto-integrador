a10d9c6f6b9309aa3e5b025cb35953f9aab7e445
lynchv-udesa <lynchv@udesa.edu.ar>
2024-04-08 15:23:34 -0300

    imagenes, db, controllers

---
+++ proyecto-integrador/db/index.js
@@ -0,0 +1,294 @@
+const data = {
+    usuario: [
+        { 
+            id: 1,
+            nombreUsuario: 'susanita',
+            email: 'susanag@gmail.com',
+            contrasenia: 'susanaal9009',
+            fechaNacimiento: '1944-01-29',
+            documento: 8274399,
+            fotoPerfil:   ,
+            },
+        {
+            id: 2,
+            nombreUsuario: 'emiliam',
+            email: 'emiliamernes@gmail.com',
+            contrasenia: '12345678',
+            fechaNacimiento: '1996-10-29',
+            documento: 35746807,
+            fotoPerfil:   ,
+            },
+        {
+            id: 3,
+            nombreUsuario: 'kimk1980',
+            email: 'kimk1980@gmail.com',
+            contrasenia: 'kimj1234',
+            fechaNacimiento: '1980-10-21',
+            documento: 26890765,
+            fotoPerfil:   ,
+            },
+        {
+            id: 4,
+            nombreUsuario: 'jamie',
+            email: 'jamescharles@gmail.com',
+            contrasenia: 'slay',
+            fechaNacimiento: '1999-05-23',
+            documento: 39243746,
+            fotoPerfil:   ,
+            },
+        {
+            id: 5,
+            nombreUsuario: 'jojosiwa',
+            email: 'jojosiwaa@gmail.com',
+            contrasenia: 'imabadgirl',
+            fechaNacimiento: '2003-05-19',
+            documento: 44938004,
+            fotoPerfil:   ,
+            },
+    ],
+
+    productos: [
+        {
+            imagen: 
+            nombreProducto: 'SACO VALENCIA',
+            descripcion: 'Saco entallado confeccionado en género sastrero, suave y agradable. Un diseño clásico modernizado, con recortes de entalle y cuello sin quiebre. Posee bolsillos sastreros con tapa, y una etiqueta tejida en la manga, exclusiva de nuestra colección. Combinalo con el Pantalon Anto para lograr el total look.' ,
+            id: 1,
+            },    
+        {
+            imagen: ,
+            nombreProducto: 'PANTALON SKY BLACK',
+            descripcion: 'El pantalón Sky está confeccionado en un género de punto compacto con spandex. El fit es ajustado al cuerpo, con tajo en la botamanga. Un definitivo must-have para día y noche. Tiene elástico en la cintura para un mejor calce, y cierre invisible en espalda.',
+            id: 2,
+            },
+        {
+            imagen: ,
+            nombreProducto: 'CAMPERA FANTASY',
+            descripcion: 'Campera bomber confeccionada en rústico metálizado.',
+            id: 3,
+            },
+        {
+            imagen: ,
+            nombreProducto: 'BODY ANA',
+            descripcion: 'Body al cuerpo confeccionado en tejido de punto suave. Cuenta con extracción en espalda y broches en entrepierna. Un item de nuestra colección que no puede faltarte!',
+            id: 4,
+            },
+        {
+            imagen: ,
+            nombreProducto: 'BLUSA KATU',
+            descripcion: 'Blusa holgada confeccionada en satén opaco combinado con género traslucido. Presenta detalle de puntilla en mangas. ',
+            id: 5,
+            },
+        {
+            imagen: ,
+            nombreProducto: 'PANTALON FL MARTI',
+            descripcion: 'Pantalón recto confeccionado en género brilloso con textura. En el delantero lleva bolsillos.',
+            id: 6,
+            },
+        {
+            imagen: ,
+            nombreProducto: 'MINI SCARLET',
+            descripcion: 'Mini drapeada confeccionada en cuero ecológico de calce al cuerpo. Acceso a la prenda por cierre lateral.',
+            id: 7,
+            },
+        {
+            imagen: ,
+            nombreProducto: 'MINI LUNA',
+            descripcion: 'Mini short confeccionada en crepe viscosa. Tiro medio. Botón exclusivo "Tucci".',
+            id: 8,
+            },
+        {
+            imagen: ,
+            nombreProducto: 'POLERA CANDE',
+            descripcion: 'Polera tejida ajustada al cuerpo con cuello alto. Un modelo clásico que no falla. El tejido tiene textura suave y abrigada. Ideal para usar de base en cualquier outfit de la temporada.',
+            id: 9,
+            },
+        {
+            imagen: ,
+            nombreProducto: 'SWEATER TWINS',
+            descripcion: 'Nuestro Sweater Twins es un conjunto de 2 piezas tejidas que se pueden usar juntas o separadas. Por un lado las mangas, y por debajo el top strapless. La silueta es ajustada al cuerpo, y el tacto es super suave y agradable. Un diseño con mucha onda para combinar con una infinidad de posibidades. En el escote de espalda presenta una badana con nuestro logo en foil metalizado.',
+            id: 10,
+            }
+    ],
+    comentarios: [
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 1,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 2,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 3,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 4,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 5,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 6,
+            }
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 7,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 8,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 9,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 10,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 11,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 12,
+            }
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 13,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 14,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 15,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 16,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 17,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 18,
+            }
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 19,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 20,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 21,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 22,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 23,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 24,
+            }
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 25,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 26,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 27,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 28,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 29,
+            },
+        {
+            nombreUsuarioC:
+            textoC:
+            imagenPerfilC: 
+            id: 30,
+            }
+        ],
+};
