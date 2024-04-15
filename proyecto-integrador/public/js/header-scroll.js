65f36d2c591a9171e8bee2286ac13633f8afd879
antonelita100 <aaxelrad@udesa.edu.ar>
2024-04-15 09:50:41 -0300

    script

--- proyecto-integrador/public/js/header-scroll.js
+++ proyecto-integrador/public/js/header-scroll.js
@@ -1,18 +1,11 @@
-// header-scroll.js
+let header = document.querySelector('header');
 
-// Seleccionar el encabezado
-var header = document.querySelector('header');
-
-// Función para cambiar el color de fondo del encabezado cuando se hace scroll
-function toggleHeaderBackground() {
+function scroll() {
   if (window.scrollY > 0) {
-    // Si se ha desplazado hacia abajo, agregar la clase 'scrolled'
     header.classList.add('scrolled');
   } else {
-    // Si se encuentra en la parte superior de la página, eliminar la clase 'scrolled'
     header.classList.remove('scrolled');
   }
 }
 
-// Escuchar el evento de scroll y llamar a la función
-window.addEventListener('scroll', toggleHeaderBackground);
+window.addEventListener('scroll', scroll);
