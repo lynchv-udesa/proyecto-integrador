e2aac500aabcce7ca057708e2dd6f4287ed30cfc
lynchv-udesa <lynchv@udesa.edu.ar>
2024-04-15 11:28:04 -0300

    header logueado, ruta users

--- proyecto-integrador/routes/index.js
+++ proyecto-integrador/routes/index.js
@@ -3,7 +3,6 @@ const indexController = require('../controllers/indexController');
 const router = express.Router();
 
 router.get('/', indexController.index);
-router.get('/profile', indexController.profile);
 //router.get('/search', indexController.search);
 router.get("/register", indexController.register);
 router.get("/login", indexController.login);
--- proyecto-integrador/routes/users.js
+++ proyecto-integrador/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require('../controllers/usersController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/profile', usersController.profile);
+
+router.get('/profile', function(req, res, next) {
+  for(let i=0; i < 1; i++){
+    return res.render('profile', {
+        nombreUsuario: `${db.usuario[i].nombreUsuario}`,
+    })
+}},
+);
 
 module.exports = router;
