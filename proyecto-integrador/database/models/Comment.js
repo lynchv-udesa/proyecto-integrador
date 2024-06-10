36453ec2cc552aac52ecb0076449be5bb80c241c
antonelita100 <aaxelrad@udesa.edu.ar>
2024-06-10 09:35:50 -0300

    Modelos de comment y product, configuracion de sequelize

--- proyecto-integrador/database/models/Comment.js
+++ proyecto-integrador/database/models/Comment.js
@@ -1,1 +1,34 @@
-module.exports = function(sequelize, datatypes){}
+module.exports = function(sequelize, datatypes){
+    let alias = "Comment";
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: datatypes.INTEGER.UNSIGNED,
+            allowNull: false
+        },
+        texto: {
+            type: datatypes.STRING,
+            allowNull: false
+        },
+        idUsuarioC: {
+            type: datatypes.INTEGER.UNSIGNED,
+            allowNull: false
+        },
+        idProducto: {
+            type: datatypes.INTEGER.UNSIGNED,
+            allowNull: false
+        },
+    }
+    }
+    
+    let config = {
+        tableName: 'productos', 
+        timestamps: true, 
+        underscored: false,
+    }
+
+   const Product = sequelize.define(alias, cols, config);
+
+   return Product;
