6d42372fe5ca84e31ea299621314dcf3a82f7790
antonelita100 <aaxelrad@udesa.edu.ar>
2024-06-17 14:28:34 -0300

    home page

--- proyecto-integrador/controllers/indexController.js
+++ proyecto-integrador/controllers/indexController.js
@@ -3,14 +3,19 @@ const db = require('../database/models');
 const indexController = {
 
     index: function (req, res) {
-        db.Product.findAll()
-            .then(products => {
-                return res.render('index', {
-                    index: products,
-                    user: req.session.user
-                });
-            })
-            .catch(error => console.log(error));
+        db.Product.findAll({
+            order: [
+                ["createdAt", "DESC"]
+            ],
+            limit: 8
+        })
+        .then(products => {
+            return res.render('index', {
+                index: products,
+                user: req.session.user
+            });
+        })
+        .catch(error => console.log(error));
     },
 
     register: function (req, res) {
