cdb0af1ddc7590eddf974b3904567bb07e944a66
lynchv-udesa <lynchv@udesa.edu.ar>
2024-06-17 00:33:37 -0300

    ya ni se

--- proyecto-integrador/controllers/loginController.js
+++ proyecto-integrador/controllers/loginController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 const db = require('../database/models');
 const { validationResult } = require("express-validator");
 
@@ -6,7 +6,7 @@ const op = db.Sequelize.Op;
 
 let loginController = {
     index: function(req, res){
-        //Mostramos el form de login
+        // Mostramos el form de login
         return res.render('login');
     },
     login: function(req, res){    
@@ -15,25 +15,60 @@ let loginController = {
             return res.render("login", { 
                 errors: resultValidation.mapped(),
                 oldData: req.body
-             })
+            });
         } else {
             db.User.findOne({
                 where: { email: req.body.email }
             })
-            .then( function ( user ) {
+            .then(function(user) {
+                if (!user) {
+                    return res.render("login", {
+                        errors: {
+                            email: {
+                                msg: "El email no está registrado"
+                            }
+                        },
+                        oldData: req.body
+                    });
+                }
+
+                // Verificar la contraseña usando bcrypt
+                const contraseniaOk = bcryptjs.compareSync(req.body.contrasenia, user.contrasenia);
+
+                console.log("Contraseña proporcionada:", req.body.contrasenia);
+                console.log("Contraseña en la base de datos:", user.contrasenia);
+                console.log("Resultado de la comparación:", contraseniaOk);
+
+                if (!contraseniaOk) {
+                    return res.render("login", {
+                        errors: {
+                            contrasenia: {
+                                msg: "Contraseña incorrecta"
+                            }
+                        },
+                        oldData: req.body
+                    });
+                }
+
                 req.session.user = user;          
                 if(req.body.checkbox != undefined){
-                    res.cookie('userId', user.id, { maxAge: 1000 * 60 * 100})
+                    res.cookie('userId', user.id, { maxAge: 1000 * 60 * 100});
                 }
                 return res.redirect('/');            
             })
-            .catch( function(e) {
-                console.log(e)
-            })
+            .catch(function(e) {
+                console.log(e);
+                return res.render("login", {
+                    errors: {
+                        email: {
+                            msg: "Error en el servidor. Por favor, intente de nuevo más tarde."
+                        }
+                    },
+                    oldData: req.body
+                });
+            });
         }
     },
-
-    
 }
 
 module.exports = loginController;
--- proyecto-integrador/routes/index.js
+++ proyecto-integrador/routes/index.js
@@ -4,11 +4,8 @@ const router = express.Router();
 
 router.get('/', indexController.index);
 router.get("/register", indexController.register);
-<<<<<<< HEAD
-=======
-router.get("/login", indexController.login);
-router.get("/add", indexController.add);
->>>>>>> 81283dfe7c505cf2086aa816b593557b4cf38bce
+//router.get("/login", indexController.login);
+//router.get("/add", indexController.add);
 
 
 
